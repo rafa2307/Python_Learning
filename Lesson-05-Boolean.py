911e46c51c6d202af2ac85abbce91caf11d5cd23
Rafael Loredo <rafaelloredo41@yahoo.com>
2025-04-10 22:49:50 -0500

    In this lesson i learned about bolleans

---
+++ Lesson-05-Boolean.py
@@ -0,0 +1,32 @@
+'''
+In this lesson i learned about booleans and logical operators and comparison operators.
+'''
+
+t = True
+
+f = False
+print(t, f)
+
+print(t and f)
+
+print(t and t)
+
+print(t or f)
+
+print(t or f or f)
+
+print(not t, not f)
+
+b = 3 == 3
+
+print(type(b), b)
+
+print(3 != 3)
+
+print(3 > 3)
+
+print(3 < 3)
+
+print(3 >= 3)
+
+print(3 <= 3)
