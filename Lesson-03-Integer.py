4de4d658340c10aa57e5b20a20ee0747dc933f6b
Rafael Loredo <rafaelloredo41@yahoo.com>
2025-04-10 20:00:04 -0500

    In this lesson i learned about integers and functions to use with them and arithmetic operators

---
+++ Lesson-03-Integer.py
@@ -0,0 +1,33 @@
+'''
+In this lesson i learned about arithmetic operators, str and type functions
+'''
+
+n1 = 5
+
+print(n1)
+
+s1 = '5'
+
+print(s1)
+
+print(type(n1)) # the type function will return the type of the variable
+
+print(type(s1))
+
+n2 = 3
+
+print(n1 + n2)
+
+print(str(n1) + s1) # The str function will turn any variable into a string
+
+print(n1 - n2) # Subtraction operator
+
+print(n1 * n2) # miltiplication operator
+
+print(n1 / n2) # division operator
+
+print(n1 // n2) # 
+
+print(n1 % n2) # Modulos uperator
+
+print(n1 ** n2) # Power operator
