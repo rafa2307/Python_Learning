779682b5831f32314ac3b5a0454206bb7760f057
Rafael Loredo <rafaelloredo41@yahoo.com>
2025-04-10 19:42:47 -0500

    My first python lesson exercises

---
+++ Lesson-01-String.py
@@ -0,0 +1,18 @@
+'''This is a 
+multiline
+comment'''
+
+print("Hello World")
+
+print('Hello', 'World')
+
+print('Hello' + ' ' + 'World')
+
+print('I\'m a string')
+
+print("I'm a string") # you can add single quotes inside a string with double quotes
+
+print('line 1\nline 2') # the \n lets you print line 2 in a new line
+
+print('\tline 3\nline 4') # the \t adds a tab to the beginning of the line
+
