d7ed5fe2a6b664cffb3f76c179f332de7e81c5ab
Rafael Loredo <rafaelloredo41@yahoo.com>
2025-04-10 19:49:38 -0500

    In this lesson i learned about the input function and variables

---
+++ Lesson-02-Variable.py
@@ -0,0 +1,17 @@
+'''
+Learned about variable and how to use the input function
+'''
+my_name = 'Red'
+
+print(my_name)
+
+print('My name is ' + my_name)
+
+name = input('Enter your name: ')
+
+print(name)
+
+greet = 'Hello'
+
+print(greet, name)
+
